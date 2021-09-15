be3e09a7d3dad29ddaa707446fc7e678151dae89
Ankoopa <dmdasalla@mcm.edu.ph>
2021-09-15 19:14:29 +0800

    added card prefab

--- Category Is/Assets/Scripts/Card.cs
+++ Category Is/Assets/Scripts/Card.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
     public List<ScriptableCard> thisCard;
 
+    [Header("Image")]
+    public Sprite cardImage;
+
+    [Header("Properties")]
+    public string CardName;
+    public int CardAmount;
+    public List<CardAbility> CardAbilities = new List<CardAbility>();
+
+    [Header("Card Description")]
+    [SerializeField, TextArea(1, 30)] public string Description;
+
     public void Start()
     {
-        
+        gameObject.GetComponent<Image>().sprite = cardImage;
     }
-    
-    // public CardInfo(ScriptableCard data)
-    // {
-
-    // }
 }
