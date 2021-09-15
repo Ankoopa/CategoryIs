d86582f308993b9ef4f48af73761b47f6f4bc608
dubz-skrrrt <chill.99.lr@gmail.com>
2021-09-15 16:24:30 +0800

    Scriptable Ability

--- Category Is/Assets/Scripts/ScriptableAbilities.cs
+++ Category Is/Assets/Scripts/ScriptableAbilities.cs
@@ -2,8 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(fileName = "NewAbility", menuName = "Ability")]
 public class ScriptableAbilities : ScriptableObject
 {
-   
+//    [Header("Targets")]
+//    public List<Target> targets = new List<Target>();
 
+    [Header ("LIFE and DEATH")]
+    public bool saved;
+    public bool dead;
+
+    [Header ("SHUFFLE & GREED")]
+    public bool shuffle;
+    public int CardsToPick = 0;
+
+    [Header ("SKIP & ROTATION")]
+    public bool clockwise;
+    public bool skip;
+
+    [Header ("STEAL & TIME")]
+    //steal code
+    public int TimeIncrease = 0;
 }
