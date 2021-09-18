e51391882c4acae312eee86666fb9a0b7044e898
Ankoopa <dmdasalla@mcm.edu.ph>
2021-09-18 23:02:56 +0800

    added random y-positioning of each card

--- Category Is/Assets/Scripts/DrawCards.cs
+++ Category Is/Assets/Scripts/DrawCards.cs
@@ -9,17 +9,29 @@ public class DrawCards : MonoBehaviour
     public GameObject playerDeck;
     public GameObject enemyDeck;
     public Card cardInfo;
-    
+
     public void BtnClick()
     {
-        for(int i = 0; i < 6; i++)
+        for(int i = -275; i <= 250; i+=105)
         {
             cardInfo.RandomizeCards();
-            GameObject playerCard = Instantiate(card, new Vector3(0, 0, 0), Quaternion.identity);
+            GameObject playerCard = Instantiate(card, new Vector3(i, -50+(generateYPos()), 0), Quaternion.identity);
             playerCard.transform.SetParent(playerDeck.transform, false);
 
             // GameObject enemyCard = Instantiate(card, new Vector3(0, 0, 0), Quaternion.identity);
             // enemyCard.transform.SetParent(enemyDeck.transform, false);
         }
+    }
+
+    int generateYPos()
+    {
+        int YPos;
+        do
+        {
+            YPos = Random.Range(-10, 10);
+        }
+        while (YPos > -5 && YPos < 5);
+
+        return YPos;
     }
 }
