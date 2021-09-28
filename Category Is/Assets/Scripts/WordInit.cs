ded10a805df0bde02342f7692ba21d1c01957560
Ankoopa <dmdasalla@mcm.edu.ph>
2021-09-28 20:31:49 +0800

    added rudimentary functions of the word game

--- Category Is/Assets/Scripts/WordInit.cs
+++ Category Is/Assets/Scripts/WordInit.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
+using UnityEngine.UI;
 
 public class WordInit : MonoBehaviour
 {
     public TextAsset textFile;
     public List<string> wordList;
+    public Text categoryText;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         var content = textFile.text;
         var allWords = content.Split('\n');
         wordList = new List<string>(allWords);
 
         Debug.Log(wordList[0]);
+    }
+
+    void Start()
+    {
+        
     }
 
     // Update is called once per frame
