b70a59852923149e9d92afe90226dd792d9f96b5
dubz-skrrrt <chill.99.lr@gmail.com>
2021-09-28 20:55:12 +0800

    BugFix-DeckBuilding

--- Category Is/Assets/Scripts/Card.cs
+++ Category Is/Assets/Scripts/Card.cs
@@ -20,4 +20,12 @@ public class Card : MonoBehaviour
     // {
     //     SetCard(CardDataBase.GetCardByID(searchCardID));
     // }
+
+    public void SetCard(ScriptableCard i)
+    {
+        CardID = i.UCardID;
+        cardFaces.sprite = i.cardImage;
+        cardAmount = i.CardAmount;
+        //Debug.Log(CardID);
+    }
 }
