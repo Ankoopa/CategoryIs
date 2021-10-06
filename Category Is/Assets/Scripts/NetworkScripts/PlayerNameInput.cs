aaf8f9dde4f2db2ab0748de582c78da571a1a917
Ankoopa <dmdasalla@mcm.edu.ph>
2021-10-06 23:24:05 +0800

    added room capabilities

--- Category Is/Assets/Scripts/NetworkScripts/PlayerNameInput.cs
+++ Category Is/Assets/Scripts/NetworkScripts/PlayerNameInput.cs
@@ -6,9 +6,11 @@ using Photon.Pun;
 using Photon.Realtime;
 
 [RequireComponent(typeof(InputField))]
-public class PlayerNameInput : MonoBehaviour
+public class PlayerNameInput : MonoBehaviourPunCallbacks
 {
     const string playerNamePrefKey = "PlayerName";
+    public Button submitBtn;
+
     InputField _inputField;
     // Start is called before the first frame update
     void Start()
@@ -49,7 +51,37 @@ public class PlayerNameInput : MonoBehaviour
         }
         else
         {
-            PhotonNetwork.LoadLevel("GameCardScene");
+            PhotonNetwork.JoinRandomRoom();
+            submitBtn.interactable = false;
         }
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        Debug.LogError("No open games available");
+        CreateRoom();
+    }
+
+    void CreateRoom()
+    {
+        int randRmName = Random.Range(1, 100);
+        RoomOptions rmOpts = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 2 };
+        PhotonNetwork.CreateRoom("Room" + randRmName, rmOpts);
+        Debug.Log("Room created: Room" + randRmName);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Player joined room");
+        base.OnJoinedRoom();
+        PhotonNetwork.LoadLevel("GameCardScene");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.LogError("Room creation failed. Trying again...");
+        base.OnCreateRoomFailed(returnCode, message);
+        CreateRoom();
     }
 }
