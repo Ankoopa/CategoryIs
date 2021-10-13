6eaccc81421d3c8312a2ccf2ccdf1c9370389e45
dubz-skrrrt <chill.99.lr@gmail.com>
2021-10-13 20:16:43 +0800

    Selection of Avatar

--- Category Is/Assets/Scripts/AvatarSelection.cs
+++ Category Is/Assets/Scripts/AvatarSelection.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class AvatarSelection : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class AvatarSelection : MonoBehaviour
     public ScriptableAvatars avatarDB;
     public Transform parent;
     public Image avatarImage;
+    public GameObject avatarImageDP;
+    public List<GameObject> avatarList;
     private static ChoosingAvatar instance;
     private int index;
     //public Text avatarName;
@@ -27,6 +30,7 @@ public class AvatarSelection : MonoBehaviour
             {
                 AddAvatarTolist(avatarDB.avatars[i]);
                 GameObject avatars = Instantiate(avatar, new Vector2(avatar.transform.position.x + 1,0), Quaternion.identity, parent);
+                avatarList.Add(avatars);
                 index++;
             }
         }
@@ -36,5 +40,20 @@ public class AvatarSelection : MonoBehaviour
     {
         avatarImage.sprite = avatar.AvatarImage;
         //avatarName.text = avatar.item_name;
+    }
+
+    public void ChangeAvatar()
+    {
+        GameObject newAvatar = EventSystem.current.currentSelectedGameObject;
+        if (newAvatar != null)
+        {
+            Debug.Log(newAvatar.GetComponent<Image>().sprite);
+            avatarImageDP.GetComponent<Image>().sprite = newAvatar.GetComponent<Image>().sprite;
+        }
+        else
+        {
+            Debug.Log("newAvatar variable is empty");
+        }
+        
     }
 }
