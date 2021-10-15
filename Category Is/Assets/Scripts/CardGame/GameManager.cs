89c6db2d07feb44defcb6283f6c70cdefa7fa44a
Don "Kerman" D <dmdasalla@mcm.edu.ph>
2021-10-15 12:35:25 +0800

    Enemy decks now instantiate based on how many players are there

--- Category Is/Assets/Scripts/CardGame/GameManager.cs
+++ Category Is/Assets/Scripts/CardGame/GameManager.cs
@@ -9,15 +9,13 @@ public class GameManager : MonoBehaviour
 {
     public GameObject card;
     public GameObject playerDeck;
+    public GameObject enemyPanel;
     public GameObject enemyDeck;
     public GameObject playerAvatar;
 
     public Card cardInfo;
     public Deck deckInfo;
     public Text playerText;
-
-    private int cardNum = 0;
-
 
     void Start()
     {
@@ -47,6 +45,18 @@ public class GameManager : MonoBehaviour
     {
         playerText.text = PhotonNetwork.LocalPlayer.NickName;
 
-        //TODO: Instantiate opponent panels.
+        //can spawn up to 3 opponents from the player list
+        foreach (Player plr in PhotonNetwork.PlayerList)
+        {
+            if (plr.IsLocal) continue;
+            else // Instantiate opponent decks on the panel and sets their name accordingly
+            {
+                Instantiate(enemyDeck, enemyPanel.transform);
+                Text enemyName = enemyDeck.transform.GetChild(0).GetChild(0).gameObject.GetComponent<Text>();
+                enemyName.text = plr.NickName;
+            }
+        }
+
+        //TODO: opponent decks have cards and avatar.
     }
 }
